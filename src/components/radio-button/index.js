e00d0fcdd50ab68731959d2792503ba8ad8c4787
zouhuigang <952750120@qq.com>
2018-08-15 09:41:30 +0800

    zantui init

---
+++ src/components/radio-button/index.js
@@ -0,0 +1,3 @@
+import RadioButton from '../radio/radio-button.vue';
+
+export default RadioButton;
---
+++ src/components/radio/index.js
@@ -0,0 +1,7 @@
+import Radio from './radio.vue';
+import RadioGroup from './radio-group.vue';
+import RadioButton from './radio-button.vue';
+
+Radio.Group = RadioGroup;
+Radio.Button = RadioButton;
+export default Radio;
